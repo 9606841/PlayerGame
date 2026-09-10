cdac1a8afc0e45d3ce07402cc3fd88ee8e2d1c22
ElisaTandra3628 <142921807+9606841@users.noreply.github.com>
2026-09-10 13:05:00 -0600

    Implement attack method in Necromancer class
    Added attack method to Necromancer class for player actions.

--- necro.py
+++ necro.py
@@ -1,1 +1,15 @@
+from player import Player
 
+class Necromancer(Player):
+    def attack(self, victim):
+        attack = input("Would you like to save yourself or attack your opponent(s/a):")
+        while attack != "s" and attack != "a":
+            attack = input("Please enter a valid option (s/a):")
+        
+        if attack == "s":
+            self.heal(10-self.health)
+            return f"{self.name} has resurrected themselves! {self.name}'s current health is {self.health}."
+        elif attack == "sl":
+            victim.takeDamage(1)
+            return f"{self.name} has attacked {victim.name}, but it wasn't very good! {victim.name}'s current health is {victim.health}."
+        
